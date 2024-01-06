3e9786ff67322f05b2fff66a89e91a64ffea346c
Prid13 <prid_outing@hotmail.com>
2024-01-06 19:49:02 +0100

    Test JSON Data

---
+++ json-data.js
@@ -0,0 +1,154 @@
+window[document.currentScript.getAttribute('data-var-name')] = JSON.stringify(
+[
+  {
+    "name": "Mortal Kombat 1",
+    "genre": [
+      "Brutal"
+    ],
+    "characters": {
+      "total_count": 23,
+      "base_count": 23,
+      "secondary_count": 15,
+      "format": "{{base}} characters + {{secondary}} secondary"
+    },
+    "price": {
+      "amount": 479,
+      "suffix": "kr",
+      "currency": "NOK"
+    },
+    "labels": [
+      {
+        "text": "New",
+        "color": "orange"
+      },
+      {
+        "text": "18+",
+        "color": "red"
+      }
+    ]
+  },
+  {
+    "name": "Mortal Kombat 11 Ultimate",
+    "genre": [
+      "Brutal"
+    ],
+    "characters": {
+      "total_count": 37,
+      "base_count": 25,
+      "dlc_count": 12,
+      "format": "{{total}} ({{base}} base + {{dlc}} DLC)"
+    },
+    "price": {
+      "amount": 109,
+      "suffix": "kr",
+      "currency": "NOK"
+    },
+    "labels": [
+      {
+        "text": "18+",
+        "color": "red"
+      }
+    ]
+  },
+  {
+    "name": "DnF Duel",
+    "genre": [
+      "Anime"
+    ],
+    "characters": {
+      "total_count": 16,
+      "format": "{{total}} characters"
+    },
+    "price": {
+      "amount": 193,
+      "suffix": "kr",
+      "currency": "NOK"
+    }
+  },
+  {
+    "name": "Dragon Ball FighterZ Ultimate",
+    "genre": [
+      "Anime",
+      "2v2"
+    ],
+    "characters": {
+      "total_count": 29,
+      "format": "{{total}} characters"
+    },
+    "price": {
+      "amount": 123,
+      "suffix": "kr",
+      "currency": "NOK"
+    }
+  },
+  {
+    "name": "Them's Fightin' Herds",
+    "genre": [
+      "Furry",
+      "Pony"
+    ],
+    "characters": {
+      "total_count": 7,
+      "format": "{{total}} characters!"
+    },
+    "price": {
+      "amount": 102,
+      "suffix": "kr",
+      "currency": "NOK"
+    }
+  },
+  {
+    "name": "Melty Blood: Type Lumina",
+    "genre": [
+      "Anime",
+      "2D Sprites",
+      "Fast-paced"
+    ],
+    "characters": {
+      "total_count": 22,
+      "format": "{{total}} characters"
+    },
+    "price": {
+      "amount": 274,
+      "suffix": "kr",
+      "currency": "NOK"
+    }
+  },
+  {
+    "name": "Power Rangers: Battle For The Grid - Super Edition",
+    "genre": [
+      "Low-budget",
+      "TV Series",
+      "Adaptation"
+    ],
+    "characters": {
+      "total_count": 26,
+      "base_count": "12",
+      "dlc_count": "14",
+      "format": "{{total}} ({{base}} base + {{dlc}} DLC)"
+    },
+    "price": {
+      "amount": 207,
+      "suffix": "kr",
+      "currency": "NOK"
+    }
+  },
+  {
+    "name": "JoJo's Bizarre Adventure: All-Star Battle R Deluxe Edition",
+    "genre": [
+      "Anime"
+    ],
+    "characters": {
+      "total_count": 55,
+      "base_count": "51",
+      "dlc_count": "4",
+      "format": "**{{base}}** base characters!! + {{dlc}} DLC"
+    },
+    "price": {
+      "amount": 279,
+      "suffix": "kr",
+      "currency": "NOK"
+    }
+  }
+]
+); // DON'T REMOVE FIRST OF LAST LINES!! Raw JSON data should always be IN-BETWEEN them!
